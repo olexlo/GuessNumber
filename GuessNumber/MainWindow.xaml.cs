e63de9c27f5278d51228e2532d5020809ae59dea
olexlo <olexiylo@gmail.com>
2023-08-23 17:23:24 +0300

    початковий етап

--- GuessNumber/MainWindow.xaml.cs
+++ GuessNumber/MainWindow.xaml.cs
@@ -20,10 +20,54 @@ namespace GuessNumber
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int limitNum = 0, upperNum = 0, lowerNum = 1000;
+        public int hiddenNum = 0;
+        public bool start_end = false, flagEnd = false;
+        Random rdm = new Random();
         public MainWindow()
         {
             InitializeComponent();
         }
-                
+
+        private void RangeTen_Click(object sender, RoutedEventArgs e)
+        {
+            limitNum = 10;
+        }
+
+        private void RangeHundred_Click(object sender, RoutedEventArgs e)
+        {
+            limitNum = 100;
+        }
+
+        private void RangeThousand_Click(object sender, RoutedEventArgs e)
+        {
+            limitNum = 1000;
+        }
+
+        private void BeginToPlay_Click(object sender, RoutedEventArgs e)
+        {
+            start_end = !start_end;
+            if (start_end) 
+            {
+                hiddenNum = rdm.Next(limitNum);
+                InformationTable.Content = "Гра почалася! Вгадай число в диапазоне від 0 до " + limitNum.ToString() + ".";
+            }
+            else 
+            {
+                InformationTable.Content = "Ви здалися. Загадане число це => " + hiddenNum.ToString();
+            }
+        }
+
+        private void ClickAndGuess_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AccidentallyCrop_Click(object sender, RoutedEventArgs e)
+        {
+            upperNum = rdm.Next(hiddenNum, limitNum+1);
+            lowerNum = rdm.Next(0, limitNum-1);
+        }
+
     }
 }
